2ea13b1d769fa68d84c920c30c00563dbbd41939
Eric Puchmayr <epuchmayr@gmail.com>
2023-06-05 10:11:39 -0700

    try earlier canvas lib to reduce bundle

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 // For building on vercel: https://github.com/Automattic/node-canvas/issues/1779
+// Error: /lib64/libz.so.1: version `ZLIB_1.2.9' not found (required by
+// /vercel/path0/node_modules/canvas/build/Release/libpng16.so.16)
 if (
     process.env.LD_LIBRARY_PATH == null ||
     !process.env.LD_LIBRARY_PATH.includes(
@@ -9,7 +11,6 @@ if (
         process.env.PWD
     }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
 }
-  
 
 module.exports = {
     reactStrictMode: true,
