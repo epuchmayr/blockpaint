0ccce5a386d3ef0ca07c1d5773798908cf3afe0d
Eric Puchmayr <epuchmayr@gmail.com>
2023-09-08 16:48:42 -0700

    update contact page with techstack

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -5,11 +5,25 @@ import Button from '../components/Button';
 export default function Contact() {
   const router = useRouter()
   return (
-    <div>
+    <div className='p-5'>
     <Button type="button" onClick={() => router.back()}>
       Click here to go back
     </Button>
+    <br />
+    Technologies:
+    <ul>
+      <li>React</li>
+      <li>Typescript</li>
+      <li>Tailwind</li>
+      <li>SCSS</li>
+      <li>Konva</li>
+      <li>Canvas</li>
+      <li>Nextjs</li>
+      <li>Vercel</li>
+      <li>Github</li>
+    </ul>
       <br />
+      <a href="https://github.com/epuchmayr/blockpaint" target='blank'>https://github.com/epuchmayr/blockpaint</a><br />
       made by EAP!
     </div>
   );
